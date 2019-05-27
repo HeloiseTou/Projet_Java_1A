c37881a9ed33825c41ce5545cbb61bb294174072
heloise <heloise.ostier@laposte.net>
2019-05-27 11:07:57 +0200

    commentaires client

--- src/Client.java
+++ src/Client.java
@@ -1,8 +1,7 @@
 
 
-/**
- * Class Client
- */
+//Classe  Client
+
 public class Client {
 
   private String nom;
@@ -22,96 +21,68 @@ public class Client {
   }
 
 
-  // Methods
+  // Méthodes :
 
+  // Méthodes d'accès :
 
-
-  //
-  // Accessor methods
-  //
-
-  /**
-   * Set the value of nom
-   * @param newVar the new value of nom
-   */
+   // Set the value of nom
   private void setNom (String newVar) {
     nom = newVar;
   }
 
-  /**
-   * Get the value of nom
-   * @return the value of nom
-   */
+    //Get the value of nom
+    // return the value of nom
   private String getNom () {
     return nom;
   }
 
-  /**
-   * Set the value of prenom
-   * @param newVar the new value of prenom
-   */
+   // Set the value of prenom
+   // param newVar the new value of prenom
   private void setPrenom (String newVar) {
     prenom = newVar;
   }
 
-  /**
-   * Get the value of prenom
-   * @return the value of prenom
-   */
+    //Get the value of prenom
+    // return the value of prenom
   private String getPrenom () {
     return prenom;
   }
 
-  /**
-   * Set the value of age
-   * @param newVar the new value of age
-   */
+  //Set the value of age
+  //param newVar the new value of age
   private void setAge (int newVar) {
     age = newVar;
   }
 
-  /**
-   * Get the value of age
-   * @return the value of age
-   */
+  //Get the value of age
+  //return the value of age
   private int getAge () {
     return age;
   }
 
-  /**
-   * Set the value of adresse
-   * @param newVar the new value of adresse
-   */
+  //Set the value of adresse
+  //param newVar the new value of adresse
   private void setAdresse (String newVar) {
     adresse = newVar;
   }
 
-  /**
-   * Get the value of adresse
-   * @return the value of adresse
-   */
+  //Get the value of adresse
+  //return the value of adresse
   private String getAdresse () {
     return adresse;
   }
 
-  /**
-   * Set the value of numeroTel
-   * @param newVar the new value of numeroTel
-   */
+  //Set the value of numeroTel
+  //param newVar the new value of numeroTel
   private void setNumeroTel (String newVar) {
     numeroTel = newVar;
   }
 
-  /**
-   * Get the value of numeroTel
-   * @return the value of numeroTel
-   */
+  //Récupère le numéro de téléphone numeroTel
+  //Renvoie le numéro de téléphone numeroTel
   private String getNumeroTel () {
     return numeroTel;
   }
 
-  //
-  // Other methods
-  //
 
 }
