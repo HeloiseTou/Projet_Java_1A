c6047a7f24ff16d7cf3507fa14bd975883368e62
heloise <heloise.ostier@laposte.net>
2019-05-27 11:47:22 +0200

    commentaires client

--- src/Client.java
+++ src/Client.java
@@ -21,35 +21,33 @@ public class Client {
   }
 
 
-   Méthodes :
+   //Méthodes d'accès :
 
-   Méthodes d'accès :
-
-    Set the value of nom
+    //Set the value of nom
   private void setNom (String nouvelle) {
     nom = nouvelle;
   }
 
-    Get the value of nom
+    //Get the value of nom
      return the value of nom
   private String getNom () {
     return nom;
   }
 
-    Set the value of prenom
-    param nouvelle the new value of prenom
+    //Set the value of prenom
+    //param nouvelle the new value of prenom
   private void setPrenom (String nouvelle) {
     prenom = nouvelle;
   }
 
-    Get the value of prenom
-     return the value of prenom
+    //Get the value of prenom
+     //return the value of prenom
   private String getPrenom () {
     return prenom;
   }
 
-  Set the value of age
-  param nouvelle the new value of age
+  //Set the value of age
+  //param nouvelle the new value of age
   private void setAge (int nouvelle) {
     age = nouvelle;
   }
@@ -60,26 +58,26 @@ public class Client {
     return age;
   }
 
-  Set the value of adresse
-  param nouvelle the new value of adresse
+  //Set the value of adresse
+  //param nouvelle the new value of adresse
   private void setAdresse (String nouvelle) {
     adresse = nouvelle;
   }
 
-  Get the value of adresse
-  return the value of adresse
+  //Get the value of adresse
+  //return the value of adresse
   private String getAdresse () {
     return adresse;
   }
 
-  Set the value of numeroTel
-  param nouvelle the new value of numeroTel
+  //Set the value of numeroTel
+  //param nouvelle the new value of numeroTel
   private void setNumeroTel (String nouvelle) {
     numeroTel = nouvelle;
   }
 
-  Récupère le numéro de téléphone numeroTel
-  Renvoie le numéro de téléphone numeroTel
+  //Récupère le numéro de téléphone numeroTel
+  //Renvoie le numéro de téléphone numeroTel
   private String getNumeroTel () {
     return numeroTel;
   }
